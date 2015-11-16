c7d3ae34927dffb2750698e15cafc5e4046b029c
wclary <wclary@mindspring.com>
2015-11-16 11:50:00 -0500

    save modifications

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used to save of a matrix, compute its inverse,
+## save the inverse, and fetch the saved inverse if the saved matrix 
+## has not been modified.
 
-## Write a short comment describing this function
+## This function is used to save of a matrix, and associate the needed 
+## functions to either set or get the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The first time cacheSolve() is called, it computes the 
+## inverse of the matrix and saves it off.  As long as the
+## matrix is not modified, it gets the stored inverse for 
+## the matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  ## determine if inverse has been saved
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ## compute the inverse
+  ## with one argument, solve() assumes the second argument
+  ## is the idenity matrix, thus computes the inverse of the
+  ## matrix
+  data <- s$get()
+  m <- solve(data,...)
+  ## saves off the inverse
+  x$setinverse(m)
+  m
 }
